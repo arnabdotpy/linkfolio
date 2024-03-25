d27c4098be968c68d8dc9c6d2e5d180db17c8071
Arnab Roy <102012992+arnabdotpy@users.noreply.github.com>
2024-03-25 20:23:35 +0530

    [updated] pfp url

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div>
       <div className='topHeader'>
-        <img className='profileImage' src='https://media.licdn.com/dms/image/D4D03AQHyFOpdaQFqAA/profile-displayphoto-shrink_400_400/0/1697461982367?e=1709769600&v=beta&t=PLbGArajIEkPYkBMPozfKOsc2iRyPgxDo80PnXDwCbg' alt='profileImage' />
+        <img className='profileImage' src='https://media.licdn.com/dms/image/D5603AQF5cGov52id2g/profile-displayphoto-shrink_200_200/0/1706943249869?e=1717027200&v=beta&t=pcFdCS9uHwNN2lXbSEWm7G6FtRTS9KInKpxVZQZaVeM' alt='profileImage' />
         <div style={{display: 'flex', flexDirection: 'row'}}>
           <p className='nameText'>Arnab Roy</p>
           <img className='blueTick' src='https://parspng.com/wp-content/uploads/2022/07/tickpng.parspng.com-4.png' alt='profileImage' />
