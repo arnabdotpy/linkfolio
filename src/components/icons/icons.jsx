8da00489c78339337ba61009b531cc2347e2360d
Arnab Roy <mr.roy.arnab@gmail.com>
2024-07-06 21:05:52 +0530

    added: more links

--- src/components/icons/icons.jsx
+++ src/components/icons/icons.jsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import { SocialIcon } from 'react-social-icons';
-import './icons.css';
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+import "./icons.css";
 
 const Icons = () => {
   // Define your social media URLs here
   const socialMediaLinks = [
-    { url: 'https://www.instagram.com/arnabroy__', network: 'instagram' },
-    { url: 'https://www.twitter.com/ArnabRoy25', network: 'twitter' },
-    // { url: 'https://www.youtube.com/example', network: 'youtube' },
-    // { url: 'https://www.linkedin.com/in/example', network: 'linkedin' },
-    // { url: 'https://www.github.com/example', network: 'github' },
-    // { url: 'https://www.pinterest.com/example', network: 'pinterest' },
-    // { url: 'https://www.medium.com/example', network: 'medium' },
-    // { url: 'https://www.soundcloud.com/example', network: 'soundcloud' },
-    // { url: 'https://www.stackoverflow.com/example', network: 'stackoverflow' },
-    // { url: 'https://www.vimeo.com/example', network: 'vimeo' },
-    // { url: 'https://www.snapchat.com/epic.arnab', network: 'snapchat' },
-    // { url: 'https://www.reddit.com/user/ArnabRoy25', network: 'reddit' },
+    { url: "https://www.instagram.com/arnabroy__", network: "instagram" },
+    { url: "https://www.twitter.com/ArnabRoy25", network: "twitter" },
+    { url: "https://discord.com/users/879653127333228564", network: "discord" },
+    { url: "https://www.reddit.com/user/ArnabRoy25/", network: "reddit" },
+    // { url: "https://www.threads.net/@arnabroy__", network: "threads" },
+    // { url: "https://diamondapp.com/u/Arnab", network: "diamond" },
   ];
 
   return (
-    <div className='socialIconWrapper'>
+    <div className="socialIconWrapper">
       {socialMediaLinks.map((link, index) => (
         <SocialIcon
           key={index}
@@ -29,9 +23,9 @@ const Icons = () => {
           target="_blank"
           rel="noopener noreferrer"
           bgColor="transparent"
-          fgColor='#F2F4FE'
-          className='socialIcon'
-          style={{ height: 40, width: 40 }}
+          fgColor="#F2F4FE"
+          className="socialIcon"
+          style={{ height: link.network == "reddit" ? 40 :  50, width: link.network == "reddit" ? 40 : 50 }}
         />
       ))}
     </div>
